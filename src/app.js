59966ba14436f05dcc8cde1f9d72a169ec42f132
vishalnikhil <vishalnikhil2002@gmail.com>
2025-06-22 01:56:11 +0530

    error handling using try catch and also an aditonal method placed at the end of code

--- src/app.js
+++ src/app.js
@@ -10,28 +10,39 @@ const app=express();
 
 //impleted auth here right agar is route pe call aa rha h to pehle check kro if this req is actually send by admin
 //do the user actually have that tocken if not sent an error write
-//write a single auth for admin and for any admin related route first this auth middle ware will be checken only then the page will be loaded
+//write a single auth for admin and for any admin related route first this auth middle ware will be checken only then the page will be loaded 
 
 
 // app.use("/admin",AdminAuth); //this is global auth middleware for all /admin route
 
      
  
-  app.get("/admin",(req,res)=>{  //auth can also be placed here but since i have a global auth i dont need it 
+  app.get("/admin",(req,res)=>{ //auth can also be placed here but since i have a global auth i dont need it 
     
-   
-   throw new Error("this is some eror ,sjbd"); //random error but it also exposes our code we will use error handler or try catch(always)
+   try{
+       throw new Error("this is some eror"); 
       
    res.send("all data sent");
+
+   }
+   //this is proper way of handling error try catch and for safety just place the error handling at then end of your code
+
+   catch(err){
+      res.status(500).send("something went wrong");
+
+   }
+   // throw new Error("this is some eror"); //random error but it also exposes our code we will use error handler or try catch(always)
+      
+   // res.send("all data sent");
   })
 
-  app.use("/",(err,req,res,next)=>{    //error handler are placed at then end and you know why js is a syncronous single-threaded lang
+//   app.use("/",(err,req,res,next)=>{    //error handler are placed at then end and you know why js is a syncronous single-threaded lang
            
-      if(err){
-           res.status(500).send("something went wrong");   //now code will not be exposed rather this response will be sent
-      }
+//       if(err){
+//            res.status(500).send("something went wrong");   //now code will not be exposed rather this response will be sent
+//       }
       
-  })
+//   })
 
   
 //   app.get("/admin/deleteUser",AdminAuth,(req,res)=>{
