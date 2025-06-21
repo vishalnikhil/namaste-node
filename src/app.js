6b767774561afe91d825c1ea017b390ad2439963
vishalnikhil <vishalnikhil2002@gmail.com>
2025-06-22 01:51:33 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 
-const {AdminAuth}=require('./Middlewares/auth')
+
 const express=require('express');
 
 const app=express();
@@ -13,18 +13,30 @@ const app=express();
 //write a single auth for admin and for any admin related route first this auth middle ware will be checken only then the page will be loaded
 
 
-app.use("/admin",AdminAuth); //this is global auth middleware for all /admin route
+// app.use("/admin",AdminAuth); //this is global auth middleware for all /admin route
 
      
  
-  app.get("/admin/getAlldata",AdminAuth,(req,res)=>{  //auth can also be placed here but since i have a global auth i dont need it 
-        res.send("all data sent");
+  app.get("/admin",(req,res)=>{  //auth can also be placed here but since i have a global auth i dont need it 
+    
+   
+   throw new Error("this is some eror ,sjbd"); //random error but it also exposes our code we will use error handler or try catch(always)
+      
+   res.send("all data sent");
+  })
+
+  app.use("/",(err,req,res,next)=>{    //error handler are placed at then end and you know why js is a syncronous single-threaded lang
+           
+      if(err){
+           res.status(500).send("something went wrong");   //now code will not be exposed rather this response will be sent
+      }
+      
   })
 
   
-  app.get("/admin/deleteUser",AdminAuth,(req,res)=>{
-        res.send("User deleted");
-  })
+//   app.get("/admin/deleteUser",AdminAuth,(req,res)=>{
+//         res.send("User deleted");
+//   })
 
 
   
